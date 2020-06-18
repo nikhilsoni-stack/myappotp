923ea101271c151088d43d4e2f0ffdeb47ed3ab0
nikhil soi <aanikhilsoni@gmail.com>
2020-06-18 23:20:03 +0530

    Updates

---
+++ precache-manifest.cbd8cdc608e8d3bbb2e9161e941097e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9b0f20257c5b723c0399d20436d89d",
+    "url": "/nikhilsoni-stack/myappotp/index.html"
+  },
+  {
+    "revision": "e0dffc5d73e504265103",
+    "url": "/nikhilsoni-stack/myappotp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/nikhilsoni-stack/myappotp/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nikhilsoni-stack/myappotp/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0dffc5d73e504265103",
+    "url": "/nikhilsoni-stack/myappotp/static/js/main.fb610d7a.chunk.js"
+  },
+  {
+    "revision": "be7d962360efacf954a0",
+    "url": "/nikhilsoni-stack/myappotp/static/js/runtime-main.d160aa8d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nikhilsoni-stack/myappotp/static/media/logo.5d5d9eef.svg"
+  }
+]);
